5324dfd2b416fd7b805846b7f7a46866ff11fe6d
Tyler Mills <113672889+totctejee@users.noreply.github.com>
2024-02-06 12:49:36 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,12 +11,44 @@ const int width  = 800;
 const int height = 800;
 
 // Write the line method here
-// line (x0, y0, x1, y1, image, colour)
+void line(int x0, int y0, int x1, int y1, TGAImage&image, TGAColor colour) {
+    bool steep = false;
+    if (abs(x0 - x1) < abs(y0 - y1)) {
+        std::swap(x0, y0);
+        std::swap(x1, y1);
+        steep = true;
+    }
+    if (x0 > x1) {
+        std::swap(x0, x1);
+        std::swap(y0, y1);
+    }
+    for (float x = x0; x <= x1, x++;) {
+        float t = (x - x0) / (x1 - x0);
+        float y = y0 * (1 - t) + y1 * t;
+        if (steep) {
+            image.set(y, x, colour);
+        }
+        else {
+            image.set(x, y, colour);
+        }
+    }
+}
 
 int main(int argc, char** argv) {
     
     TGAImage image(width, height, TGAImage::RGB);
-
+    for (int i = 0; i < model->nfaces(); i++) {
+        std::vector<int> face = model->face(i);
+        for (int j = 0; j < 3; j++) {
+            Vec3f v0 = model->vert(face[j]);
+            Vec3f v1 = model->vert(face[(j + 1) % 3]);
+            int x0 = (v0.x + 1.) * width / 2.;
+            int y0 = (v0.y + 1.) * height / 2.;
+            int x1 = (v1.x + 1.) * width / 2.;
+            int y1 = (v1.y + 1.) * height / 2.;
+            line(x0, y0, x1, y1, image, white);
+        }
+    }
     image.flip_vertically(); // we want to have the origin at the left bottom corner of the image
     image.write_tga_file("output.tga");
     delete model;
